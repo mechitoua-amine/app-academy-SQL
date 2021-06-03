1786110f2188bb539878d92c33edda7300632218
mechitoua_amine <mechitoua_amine@protonmail.ch>
2021-06-03 22:28:56 +0100

    :poop: teachers_and_depts
    completing teachers_and_depts query

--- SQL Fundamentals/projects/SQL Zoo/lib/08_using_null.rb
+++ SQL Fundamentals/projects/SQL Zoo/lib/08_using_null.rb
@@ -74,6 +74,13 @@ def teachers_and_depts
   # department name. Use the string 'None' where there is no
   # department.
   execute(<<-SQL)
+    SELECT
+      teachers.name,
+      COALESCE(depts.name, 'None')
+    FROM
+      teachers
+    LEFT OUTER JOIN
+      depts ON teachers.dept_id = depts.id;
   SQL
 end
 
